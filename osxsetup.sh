862e25c5ddf81dcd5670f548ef32d461ef70dadd
xdccrlz <alexhide@gmail.com>
2021-10-09 08:19:18 +0300

    update script

---
+++ osxsetup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Super Mario 64 PC on macOS X
+# ToDo: Test on more Mac models with actual osX and allow existing src folders to be updated.
+#
+#!/bin/sh
+#
+# build code with enhanced options
+#
+gmake -j4 OSX_BUILD=1 VERSION=us EXTERNAL_DATA=1 NODRAWDISTANCE=1 BETTERCAMERA=1
+cd build/us_pc && ./sm64.us.f3dex2e
