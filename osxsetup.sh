61857a436ea2e19906aa10da81e9e1567e1bf506
xdccrlz <alexhide@gmail.com>
2021-10-10 15:06:26 +0300

    Update osxsetup.sh

--- osxsetup.sh
+++ osxsetup.sh
@@ -6,5 +6,5 @@
 #
 # build code with enhanced options
 #
-gmake -j4 OSX_BUILD=1 VERSION=us EXTERNAL_DATA=1 NODRAWDISTANCE=1 BETTERCAMERA=1
+gmake -j4 OSX_BUILD=1 VERSION=us NODRAWDISTANCE=1 BETTERCAMERA=1
 cd build/us_pc && ./sm64.us.f3dex2e
